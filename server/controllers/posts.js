75b88d2486dc7e136e42d1885f27aa5db89bc930
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2022-07-21 11:50:44 +0200

    initial commit

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -10,3 +10,71 @@ export const getPosts = async (req,res) => {
         })
     }
 }
+
+export const getSinglePost = async (req,res) => {
+    try {
+        const { id : _id } = req.params;
+        const post = await Post.findById(_id)
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        })
+    }
+}
+
+export const createPost = async (req,res) => {
+
+    const newPost = new Post(req.body)
+
+    try {
+
+     await  newPost.save();
+     res.status(201).json(newPost)
+        
+    } catch (error) {
+
+        res.status(409).json({
+            message: error.message
+            
+        })
+    }
+}
+export const updatePost = async (req,res) => {
+
+    const { id: _id } = req.params;
+    const post = req.body
+
+    try {
+
+      const updatedPost = await Post.findByIdAndUpdate(_id, post, { new: true });
+      res.json(updatedPost);
+
+    } catch (error) {
+
+        res.status(409).json({
+            message: error.message
+            
+        })
+        
+    }
+}
+
+export const deletePost = async (req,res) => {
+
+    const { id: _id } = req.params;
+
+    try {
+
+        const deletedPost = await Post.findByIdAndRemove(_id)
+        res.json(deletedPost);
+        
+    } catch (error) {
+
+        res.status(409).json({
+            message: error.message
+            
+        })
+        
+    }
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { getPosts } from "../controllers/posts.js"
+import { getPosts, createPost, updatePost, getSinglePost, deletePost } from "../controllers/posts.js"
 
 const router = express.Router();
 
 // http://localhost:7000/posts
 // GET POST DELETE UPDATE PATCH
 router.get("/", getPosts)
-
+router.get("/:id", getSinglePost)
+router.post("/", createPost);
+router.patch("/:id", updatePost)
+router.delete("/:id", deletePost)
 
 export default router;
